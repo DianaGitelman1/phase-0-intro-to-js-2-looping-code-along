b5a7b502e8669ccb06e96754aa9cf336ba0d3e59
Diana Gitelman <diana4kany@gmail.com>
2022-04-28 23:39:30 -0400

    commit tree a77c28ca7e2376f3e32ddfc1366a780dc13939a8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const gifts = [];
+    for (let i = 0; i < name.length; i++) {
+        gifts.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return gifts;
+}
+writeCards();
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
